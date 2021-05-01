0df58b9befee77b0c7e317f7425e007bd25dc001
Ruben Dragland <ruben.s.dragland@gmail.com>
2021-05-01 22:08:45 +0200

    'Initilising'

---
+++ ExamPlots.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 22:07:25 2021
+
+@author: Bruker
+"""
+
+
+#Here the plot functions are to be defined and then imported to the data analysis file. 
