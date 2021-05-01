8845efa6342e3acaf263fabca93868f51286505a
Ruben Dragland <ruben.s.dragland@gmail.com>
2021-05-01 22:04:23 +0200

    'Structring'

--- ExamImplementation.py
+++ ExamImplementation.py
@@ -4,3 +4,28 @@ Created on Sat May  1 21:28:11 2021
 
 @author: Bruker
 """
+
+
+import numpy as np
+from numpy.linalg import norm
+import scipy as sp
+from scipy.sparse import diags
+from scipy.sparse.linalg import spilu, LinearOperator, bicgstab, gcrotmk
+from scipy.integrate import simps, romb
+import heapq
+
+from time import time
+from tqdm import trange, tqdm
+from numba import jit
+
+import matplotlib
+from matplotlib import pyplot as plt
+from celluloid import Camera
+
+import sys
+import logging
+
+
+#The general functions for the simulation.
+#Tasks and plots are done in separate files. 
+
